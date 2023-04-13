aab8ba004df2848bcb2918399cbaaf492b23e171
Room101o <oussamaelkaoun1@gmail.com>
2023-04-13 15:08:18 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - memory
+ * @b: ...
+ *
+ * Return: null
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+	if (x == NULL)
+		exit(98);
+	return (x);
+}
